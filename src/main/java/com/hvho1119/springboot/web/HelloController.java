1079ea7019b554b30ec123c29c4c2356e453b428
HVHO <hvho1119@gmail.com>
2020-02-05 21:21:35 +0900

    apply lombok & add DTO

--- src/main/java/com/hvho1119/springboot/web/HelloController.java
+++ src/main/java/com/hvho1119/springboot/web/HelloController.java
@@ -1,14 +1,26 @@
 package com.hvho1119.springboot.web;
 
+import com.hvho1119.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String Hello () {
-        return "Hello";
+    public String hello() {
+        return "hello";
     }
+
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto (
+            @RequestParam("name") String name,
+            @RequestParam("amount") int amount
+    ) {
+        return new HelloResponseDto(name,amount);
+    }
+
 
 }
