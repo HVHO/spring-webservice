370a47f093e0cfb3d83912f49e38af64ec9a998d
HVHO <hvho1119@gmail.com>
2020-02-22 23:39:19 +0900

    implement google login

--- src/main/java/com/hvho1119/springboot/web/IndexController.java
+++ src/main/java/com/hvho1119/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.hvho1119.springboot.web;
 
+import com.hvho1119.springboot.config.auth.dto.SessionUser;
+import com.hvho1119.springboot.domain.user.User;
 import com.hvho1119.springboot.service.posts.PostsService;
 import com.hvho1119.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +11,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @Autowired
     private PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        User user = (User) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
@@ -34,6 +45,8 @@ public class IndexController {
         model.addAttribute("post",dto);
         return "posts-update";
     }
+
+
 
 
 
